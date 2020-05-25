1cca84003cd69b580e487be88cff5d7604e1b9e8
Maria <mb.lev17@gmail.com>
2020-05-24 22:54:12 -0500

    upload data

--- specific_controllers/movie_controller/src/bin/load_data.rs
+++ specific_controllers/movie_controller/src/bin/load_data.rs
@@ -8,8 +8,49 @@ use diesel::pg::PgConnection; //Mantiene la conexion
 use csv;
 
 fn main() {
-    let pg_conn = PgConnection::establish("postgres://maria:@localhost/movie_database")
-        .expect("Error connection");
+    
+    let mut content = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_path("./Movie_Ratings.csv")
+        .expect("Couldn't load from csv file");
+    
+    let mut matrix = Vec::new();
+
+    for r in content.records() {
+        if let Ok(record) = r {
+            let mut row = Vec::new();
+            for r in record.iter() {
+                row.push(String::from(r));
+            }
+            matrix.push(row);
+        }
+    }
+
+    let connector = PgConnection::establish("postgres://maria:@localhost/movie_database").expect("Failed connection to database. Maybe the URL?");
+
+    let mut db_users = Vec::new();
+
+    for col in 1..matrix[0].len() {
+        db_users.push(create_user(&connector, &matrix[0][col]));
+    }
+
+    let mut db_movies = Vec::new();
+
+    for row in 1..matrix.len() {
+        db_movies.push(create_movie(&connector, &matrix[row][0]));
+    }
+
+
+    for i in 0..db_users.len() {
+        let current_user = &db_users[i];
+
+        for j in 0..db_movies.len() {
+            let current_movie = &db_movies[j];
+            if matrix[j+1][i+1] != "" {
+                create_score(&connector, matrix[j+1][i+1].parse().expect("Failed to parse"), current_user.id, current_movie.id);  
+            }
+        }
+    }
 }
 
 fn create_user(conn: &PgConnection, name:&String) -> FoundUser{
